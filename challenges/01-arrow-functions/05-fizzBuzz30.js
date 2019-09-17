2159508dbb3fac0ab430ea1130698f4639e777ec
Willie <willie.anima@gmail.com>
2019-09-16 20:54:23 -0700

    Finished 06-fizzBuzz30.js assignment

--- challenges/01-arrow-functions/05-fizzBuzz30.js
+++ challenges/01-arrow-functions/05-fizzBuzz30.js
@@ -2,9 +2,34 @@
 //1 through 30 replacing numbers divisible by 3 with 'fizz', numbers divisible by 5 with 'buzz' and numbers divisible by 3 and 5 with
 //'fizzbuzz'
 
-let fizzBuzz30;
+/*let fizzBuzz30;*/
 
+const fizzBuzz30 = () => {
+    let fizz = "fizz";
+    let buzz = "buzz";
 
+    let output = [];
+
+    for (let x = 0; x < 31; x++) {
+
+        if (x % 15 === 0 ) {
+            console.log(fizz+buzz);
+            output.push(fizz+buzz);
+        } else if (x % 3 === 0) {
+
+            console.log(fizz);
+            output.push(fizz);
+        } else if (x % 5 === 0) {
+
+            console.log(buzz);
+            output.push(buzz);
+        }
+    }
+
+    console.log(output);
+
+    return output;
+};
 
 
 
